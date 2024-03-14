e5e53e6112c23dd5c0dfab4f786e8ef2c3963d50
Karey Powell <kareypowell@gmail.com>
2024-03-13 22:59:33 -0500

    Add routes and handlers

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,9 +1,70 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/kareypowell/go-movies-back-end/internal/models"
 )
 
 func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
 
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *Application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		MPAARating:  "R",
+		RunTime:     116,
+		Description: "A very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	movies = append(movies, highlander)
+
+	rd, _ = time.Parse("2006-01-02", "1986-03-07")
+	raidersOfTheLostArk := models.Movie{
+		ID:          1,
+		Title:       "Raiders of the Lost Ark",
+		ReleaseDate: rd,
+		MPAARating:  "PG-13",
+		RunTime:     116,
+		Description: "Another very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	movies = append(movies, raidersOfTheLostArk)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,8 +11,10 @@ func (app *Application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.enableCORS)
 
 	mux.Get("/", app.Home)
+	mux.Get("/movies", app.AllMovies)
 
 	return mux
 }
